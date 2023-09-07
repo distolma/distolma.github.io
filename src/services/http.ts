652667cc4814b200559886f4fd57129d79dfe684
Dmytro Mostovyi <me@dmostovyi.com>
2023-09-07 11:38:18 +0200

    Add Astro

--- src/services/http.ts
+++
@@ -1,9 +0,0 @@
-export function get<T>(input: RequestInfo): Promise<T> {
-  const headers = new Headers();
-  headers.append(
-    'Authorization',
-    `Client-ID ${process.env.UNSPLASH_CLIENT_ID}`,
-  );
-
-  return fetch(input, { headers }).then((response) => response.json());
-}
