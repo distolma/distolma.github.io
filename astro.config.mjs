7442549822ddc480015dfd845df140534e4914c6
Dmytro Mostovyi <me@dmostovyi.com>
2023-12-08 18:59:48 +0100

    Add vite-plugin-mkcert for HTTPS support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import mkcert from "vite-plugin-mkcert";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dmostovyi.com',
   integrations: [tailwind()],
+  vite: {
+    plugins: [
+      mkcert()
+    ],
+    server: {
+      https: true
+    },
+  },
 });
