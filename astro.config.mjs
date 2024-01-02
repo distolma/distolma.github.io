5c678744e68b0f07e49afb3a64148d2d132d85c9
Dmytro Mostovyi <me@dmostovyi.com>
2024-01-02 09:52:26 +0100

    update astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import mkcert from "vite-plugin-mkcert";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dmostovyi.com",
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   vite: {
     plugins: [mkcert()],
     server: {
